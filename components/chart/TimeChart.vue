b8c307299ff5fc8eeb655d29e13c959a19927a2e
Bo <bo.robbrecht@student.howest.be>
2021-11-30 17:40:42 +0100

    Converted Chart into component

---
+++ components/chart/TimeChart.vue
@@ -0,0 +1,48 @@
+<template>
+  <highchart :options="chartOptions" />
+</template>
+
+<script>
+export default {
+  name: 'TimeChart',
+  props: {
+    series: {
+      // eslint-disable-next-line no-array-constructor
+      default: new Array(
+        { x: new Date('November 1, 2021 07:45:00'), y: 30 },
+        { x: new Date('November 1, 2021 08:00:00'), y: 50 }
+      ),
+      type: Array,
+    },
+    title: {
+      default: "Chart",
+      type: String
+    }
+  },
+  data() {
+    return {
+      chartOptions: {
+        colors: ['#FF7A00'],
+        credits: { enabled: false },
+        title: {
+          text: this.title,
+          style: {
+            'font-family': "'Inter', sans-serif;",
+          },
+        },
+        yAxis: {
+          labels: {
+            format: '{text} km/h',
+          },
+        },
+        xAxis: {
+          labels: {
+            format: '{value: %H:%M}',
+          },
+        },
+        series: this.series,
+      },
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,68 @@
 <template>
-    <main>
-        <HeroBanner/>
-        <article>
-            <highchart :options="chartOptions" />
-        </article>
-    </main>
+  <main>
+    <hero-banner />
+    <article>
+      <time-chart :series="series" title="Avg. Speed on Last Ride"/>
+    </article>
+  </main>
 </template>
 
 <script>
+import TimeChart from '~/components/chart/TimeChart.vue'
+import HeroBanner from '~/components/HeroBanner.vue'
 export default {
+  components: { TimeChart, HeroBanner },
   data() {
-      return {
-        chartOptions: {
-          colors: ["#FF7A00"],
-            credits: {enabled: false},
-            title: {
-                text: "test",
-                style: {
-                  "font-family": "'Inter', sans-serif;"
-                }
+    return {
+      series: [
+        {
+          name: 'Avg. Speed',
+          data: [
+            {
+              x: new Date('November 1, 2021 07:30:00'),
+              y: 0,
             },
-            yAxis: {
-              labels:{
-                format: '{text} km/h'
-              }
+            {
+              x: new Date('November 1, 2021 07:45:00'),
+              y: 30,
             },
-            xAxis: {
-              labels:{
-                format: '{value: %H:%M}',
-              }
+            {
+              x: new Date('November 1, 2021 08:00:00'),
+              y: 50,
             },
-            series: [{
-              name: 'Avg. Speed',
-              data:[{
-                x: new Date('November 1, 2021 07:30:00'),
-                y: 0,
-              },
-              {
-                x:new Date('November 1, 2021 07:45:00'),
-                y: 30,
-              },
-              {
-                x:new Date('November 1, 2021 08:00:00'),
-                y:50
-              },
-              {
-                x:new Date('November 1, 2021 08:15:00'),
-                y:45
-              },
-              {
-                x:new Date('November 1, 2021 08:30:00'),
-                y: 90
-              },
-              {
-                x:new Date('November 1, 2021 08:45:00'),
-                y: 120
-              },
-              {
-                x: new Date('November 1, 2021 09:00:00'),
-                y: 140
-              },
-              {
-                x: new Date('November 1, 2021 09:15:00'),
-                y: 50
-              },
-              {
-                x: new Date('November 1, 2021 09:30:00'),
-                y: 0
-              }
-              ]
-            }]
-        }
-      };
+            {
+              x: new Date('November 1, 2021 08:15:00'),
+              y: 45,
+            },
+            {
+              x: new Date('November 1, 2021 08:30:00'),
+              y: 90,
+            },
+            {
+              x: new Date('November 1, 2021 08:45:00'),
+              y: 120,
+            },
+            {
+              x: new Date('November 1, 2021 09:00:00'),
+              y: 140,
+            },
+            {
+              x: new Date('November 1, 2021 09:15:00'),
+              y: 50,
+            },
+            {
+              x: new Date('November 1, 2021 09:30:00'),
+              y: 0,
+            }],
+        },
+      ],
     }
+  },
 }
 </script>
 
 <style lang="scss">
-article:not(:first-of-type){
-    background-color: ghostwhite;
+article:not(:first-of-type) {
+  background-color: ghostwhite;
 }
 </style>
