4a597569822b3bbbd49dc8ce7505aa6018e1e200
Bo <bo.robbrecht@student.howest.be>
2022-01-07 13:59:11 +0100

    Made selecting component for uuid

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
   <main>
+    <article class="card-selection">
+      <p>
+        Displaying information for
+        <select id="uuid" name="uuid" @change="setUUID($event)">
+          <option value="bc-87-72-17">Card 1</option>
+          <option value="ed-36-o2-po">Card 2</option>
+          <option value="zc-19-mb-a1">Card 3</option>
+        </select>
+      </p>
+    </article>
     <hero-banner />
-    <overview :title="'Last 24 hours'" :from="from" :to="to" :interval="(60*30)"/>
+    <overview
+      :title="'Last 24 hours'"
+      :from="from"
+      :to="to"
+      :interval="60 * 30"
+    />
   </main>
 </template>
 
@@ -11,15 +26,30 @@ import HeroBanner from '~/components/HeroBanner.vue'
 export default {
   components: { HeroBanner, Overview },
   data() {
-    return{
+    return {
       to: new Date(),
-      from: new Date(new Date().setDate(new Date().getDate() - 1))
+      from: new Date(new Date().setDate(new Date().getDate() - 1)),
+      uuid: 'bc-87-72-17',
     }
-  }
+  },
+  methods: {
+    setUUID(e) {
+      this.uuid = e.target.value
+    },
+  },
 }
 </script>
 
 <style lang="scss">
+.card-selection{
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+  width: fit-content;
+  select{
+    border: none;
+  }
+}
 article:not(:first-of-type) {
   background-color: ghostwhite;
   border-radius: 10px;
